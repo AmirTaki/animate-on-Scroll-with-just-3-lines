9729ea84c4ad0172369140d0008a719b96ee7639
amir <amirtaki623@gmail.com>
2025-08-10 22:43:40 +0330

    colors block part 5

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 const colors = ['rgb(1, 4,2)', 'rgb(26, 190, 81)', 'rgb(25, 164, 62)', 'rgb(70, 255, 243)', 'rgb(27, 66, 64)' 
     ,'rgb(32, 53, 1)','rgb(161, 5, 156)', 'rgb(138, 6, 57)','rgb(151, 14, 28)',
-    "rgb(151, 14, 14)", 'rgb(206, 216, 5)','rgb(89, 216, 5)'
+    "rgb(151, 14, 14)", 'rgb(206, 216, 5)','rgb(89, 216, 5)', "silver", "blue", "pink", "orange",
+    
 ]
 function getRandomArbitrary(min, max) {
   return Math.floor( Math.random() * (max - min) + min);
@@ -9,5 +10,6 @@ const blocks = document.querySelectorAll(".block")
 blocks.forEach(block => {
     const random = (getRandomArbitrary(0, colors.length -1))
     block.style.backgroundColor = colors[random]
+
 });
 
