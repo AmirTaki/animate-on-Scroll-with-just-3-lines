a6106966f8077c993db0036c54f403bf48071248
amir <amirtaki623@gmail.com>
2025-08-10 22:41:07 +0330

    colors block part 3

--- src/main.js
+++ src/main.js
@@ -2,8 +2,12 @@ const colors = ['rgb(1, 4,2)', 'rgb(26, 190, 81)', 'rgb(25, 164, 62)', 'rgb(70, 
     ,'rgb(32, 53, 1)','rgb(161, 5, 156)', 'rgb(138, 6, 57)','rgb(151, 14, 28)',
     "rgb(151, 14, 14)", 'rgb(206, 216, 5)','rgb(89, 216, 5)'
 ]
-
+function getRandomArbitrary(min, max) {
+  return Math.floor( Math.random() * (max - min) + min);
+}
 const blocks = document.querySelectorAll(".block")
 blocks.forEach(block => {
     
 });
+
+const random = (getRandomArbitrary(0, colors.length -1))
