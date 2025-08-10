d4023f08d884a4415fd280e282693a3953d95008
amir <amirtaki623@gmail.com>
2025-08-10 22:38:12 +0330

    colors block part 2

--- src/main.js
+++ src/main.js
@@ -2,3 +2,8 @@ const colors = ['rgb(1, 4,2)', 'rgb(26, 190, 81)', 'rgb(25, 164, 62)', 'rgb(70, 
     ,'rgb(32, 53, 1)','rgb(161, 5, 156)', 'rgb(138, 6, 57)','rgb(151, 14, 28)',
     "rgb(151, 14, 14)", 'rgb(206, 216, 5)','rgb(89, 216, 5)'
 ]
+
+const blocks = document.querySelectorAll(".block")
+blocks.forEach(block => {
+    
+});
